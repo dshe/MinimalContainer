dd5ed1ca465aa8d0c25849a0dc663c0ad22fedb5
David <david@tan.com>
2016-02-12 15:19:18 +0800

    new tests

---
+++ InternalContainer.Tests/Generic/AutoGenericTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace InternalContainer.Tests.Generic
+{
+    public class AutoGenericTests
+    {
+        internal interface IClassA {}
+        internal class ClassA : IClassA {}
+        internal class ClassB<T>
+        {
+            public ClassB(T t) {}
+        }
+
+        internal class ClassC
+        {
+            public ClassC(ClassB<ClassA> ba)
+            { }
+        }
+        internal class ClassD
+        {
+            public ClassD(ClassB<IClassA> ba)
+            { }
+        }
+
+        private readonly Container container;
+        private readonly ITestOutputHelper output;
+
+        public AutoGenericTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            container = new Container(Lifestyle.Singleton, log:output.WriteLine, assemblies:Assembly.GetExecutingAssembly());
+        }
+
+        [Fact]
+        public void Test_01()
+        {
+            container.GetInstance<ClassC>();
+            Assert.Equal(3, container.GetRegistrations().Count);
+            output.WriteLine(Environment.NewLine + container);
+        }
+
+        [Fact]
+        public void Test_02()
+        {
+            container.GetInstance<ClassD>();
+            Assert.Equal(3, container.GetRegistrations().Count);
+            output.WriteLine(Environment.NewLine + container);
+        }
+
+
+
+    }
+}
--- InternalContainer/InternalContainer.cs
+++ InternalContainer/InternalContainer.cs
@@ -1,4 +1,4 @@
-﻿//InternalContainer.cs 1.10
+﻿//InternalContainer.cs 1.11
 //Copyright 2016 David Shepherd. Licensed under the Apache License 2.0: http://www.apache.org/licenses/LICENSE-2.0
 using System;
 using System.Collections;
