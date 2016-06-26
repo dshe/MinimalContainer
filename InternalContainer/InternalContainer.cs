b2b8bec7931d1b3aad58ed073996332684e969b0
David <david@tan.com>
2016-06-27 05:01:04 +0800

    constructor attribute

--- InternalContainer/InternalContainer.cs
+++ InternalContainer/InternalContainer.cs
@@ -1,4 +1,4 @@
-﻿//InternalContainer.cs 1.21
+﻿//InternalContainer.cs 1.22
 //Copyright 2016 David Shepherd. Licensed under the Apache License 2.0: http://www.apache.org/licenses/LICENSE-2.0
 
 using System;
