a4af9ce235b5e3cc7badf3d087510ad698d77034
David S. <david@tan.com>
2016-10-27 23:00:56 +0800

    src dir

--- src/StandardContainer.Tests/Examples/Example2.cs
+++ src/StandardContainer.Tests/Examples/Example2.cs
@@ -10,22 +10,23 @@ namespace StandardContainer.Tests.Examples
 {
     public class Example2
     {
+        internal interface IFoo1 {}
         internal interface IFoo2 {}
+        internal class Foo1 : IFoo1 {}
         internal class Foo2 : IFoo2 {}
-
-        internal class Foo1
-        {
-            internal Foo1(IFoo2 foo2) {}
-        }
-
         internal class Root
         {
-            internal Root(Foo1 foo1) {}
+            private readonly IFoo1 foo1;
+            private readonly Func<IFoo2> foo2Factory;
+            internal Root(IFoo1 foo1, Func<IFoo2> foo2Factory)
+            {
+                this.foo1 = foo1;
+                this.foo2Factory = foo2Factory;
+            }
             private void StartApplication()
             {
                 //...
             }
-
             public static void Main()
             {
                 new Container(DefaultLifestyle.Transient)
